618de64ca1696c02c88469db5f8fdfd0f60ed4cb
Bisnu Chandra Roy <bisnuchandraroy10@gmail.com>
2025-08-25 16:20:25 +0600

    Update functionality done

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
     app.delete("/myplants/:id", async(req, res)=>{
       const id = req.params.id;
       const result = await collection.deleteOne({
-        _id : new ObjectId(id)
+        _id : new ObjectId(id) 
       })
       res.send(result);
     })
